4b95ef132aaefe99d0fd163aab7dc9fa00ef5644
choijiwon <gwonwon1201@gmail.com>
2023-03-15 10:40:31 +0900

    [SETTING] 백엔드 프로젝트 생성

---
+++ src/main/java/com/ssafy/ChallenMungs/Test/controller/testController.java
@@ -0,0 +1,18 @@
+package com.ssafy.ChallenMungs.Test.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class testController {
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+}
