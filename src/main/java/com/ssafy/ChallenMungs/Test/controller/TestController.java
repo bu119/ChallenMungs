7965a48c59eaee5eefaee64ceb0f2242e2932c3e
choijiwon <gwonwon1201@gmail.com>
2023-03-20 20:53:06 +0900

    [SETTING] querydsl 설정

--- src/main/java/com/ssafy/ChallenMungs/Test/controller/TestController.java
+++ src/main/java/com/ssafy/ChallenMungs/Test/controller/TestController.java
@@ -21,6 +21,7 @@ public class TestController {
 
     private final TestService service;
 
+    //db와 상호작용 필요없는 일반 api
     @GetMapping("/hello")
     @ApiOperation(value = "헬로우 월드", notes = "역사적인 첫 커밋이에요!") //notes는 안적어도 상관없어요
     public String Hello(){
@@ -28,6 +29,7 @@ public class TestController {
     }
 
 
+    //jpa가 기본적으로 제공하는 함수를 이용해 db와 상호작용
     @GetMapping("/testDefault")
     @ApiOperation(value = "jpa 기본 동작 확인", notes = "jpa 기본 쿼리 메서드 확인용 - 테스트 테이블의 칼럼 수를 반환한다.")
     public long getCount(){
@@ -35,10 +37,20 @@ public class TestController {
     }
 
 
+    //jpa가 기본적으로 제공하지 않지만, 비교적 간단한 쿼리문인 경우
     @GetMapping("/testCustom")
     @ApiOperation(value = "jpa 기본 동작 확인", notes = "jpa 커스텀 쿼리 메서드 확인용 - 이름에 '안녕'을 포함하는 칼럼 리스트를 반환")
     public List<TestDto> getTest(){
         List<Test> list = service.getCustom("안녕");
+        return list.stream()
+                .map(b -> new TestDto(b.getName()))
+                .collect(Collectors.toList());
+    }
+
+    //jpa가 기본적으로 제공하지 않고, 비교적 복잡한 쿼리문인 경우
+    @GetMapping("/testQueryDsl")
+    public List <TestDto> getQueryDsl(){
+        List<Test> list = service.getQueryDsl();
         return list.stream()
                 .map(b -> new TestDto(b.getName()))
                 .collect(Collectors.toList());
--- src/main/java/com/ssafy/ChallenMungs/Test/service/TestServiceImpl.java
+++ src/main/java/com/ssafy/ChallenMungs/Test/service/TestServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ssafy.ChallenMungs.Test.service;
 
 import com.ssafy.ChallenMungs.Test.entity.Test;
+import com.ssafy.ChallenMungs.Test.repository.QuerydslRepository;
 import com.ssafy.ChallenMungs.Test.repository.TestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,13 +14,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService{
     private final TestRepository jpaRepo;
+    private final QuerydslRepository queryRepo;
     @Override
     public long getCount() {
         return jpaRepo.count();
     }
-
     @Override
     public List<Test> getCustom(String name) {
         return jpaRepo.findByNameContaining(name);
     }
+    @Override
+    public List<Test> getQueryDsl() {
+        return queryRepo.getList();
+    }
+
 }
