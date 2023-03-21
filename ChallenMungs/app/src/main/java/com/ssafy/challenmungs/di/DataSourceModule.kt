e248c57dad17835609f9157af8f44e1b43b63eda
dobbi030 <dmstjs7047@hanmail.net>
2023-03-21 17:47:37 +0900

    [FEAT] kakao login API 연동

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/di/DataSourceModule.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/di/DataSourceModule.kt
@@ -1,10 +1,20 @@
 package com.ssafy.challenmungs.di
 
+import com.ssafy.challenmungs.data.remote.datasource.auth.AuthRemoteDataSourceImpl
+import com.ssafy.challenmungs.data.remote.service.AuthApiService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DataSourceModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthDataSource(
+        authApiService: AuthApiService
+    ): AuthRemoteDataSourceImpl = AuthRemoteDataSourceImpl(authApiService)
 }
--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/di/RepositoryModule.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/di/RepositoryModule.kt
@@ -1,10 +1,21 @@
 package com.ssafy.challenmungs.di
 
+import com.ssafy.challenmungs.data.remote.datasource.auth.AuthRemoteDataSourceImpl
+import com.ssafy.challenmungs.data.remote.repository.AuthRepositoryImpl
+import com.ssafy.challenmungs.domain.repository.AuthRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(
+        authRemoteDataSourceImpl: AuthRemoteDataSourceImpl
+    ): AuthRepository = AuthRepositoryImpl(authRemoteDataSourceImpl)
 }
