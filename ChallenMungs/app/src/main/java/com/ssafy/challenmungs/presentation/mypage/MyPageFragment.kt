346d7a20170cb49037de1905cb64be8d6e2457e8
이아름 <souk0712@naver.com>
2023-03-22 02:23:15 +0900

    [CHORE] 마이페이지 layout 코드 컨벤션에 맞춰 수정

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/MyPageFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/MyPageFragment.kt
@@ -1,7 +1,5 @@
 package com.ssafy.challenmungs.presentation.mypage
 
-import android.os.Bundle
-import android.view.View
 import com.ssafy.challenmungs.R
 import com.ssafy.challenmungs.databinding.FragmentMyPageBinding
 import com.ssafy.challenmungs.presentation.base.BaseFragment
@@ -10,13 +8,5 @@ class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_
 
     override fun initView() {
 
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
     }
 }
