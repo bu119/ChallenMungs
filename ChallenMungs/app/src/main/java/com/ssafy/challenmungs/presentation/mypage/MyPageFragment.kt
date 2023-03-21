4c19f0f1add0321f29227924fa22265181313a18
이아름 <souk0712@naver.com>
2023-03-21 22:07:27 +0900

    [FEAT] 마이페이지 화면 개발

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/MyPageFragment.kt
@@ -0,0 +1,22 @@
+package com.ssafy.challenmungs.presentation.mypage
+
+import android.os.Bundle
+import android.view.View
+import com.ssafy.challenmungs.R
+import com.ssafy.challenmungs.databinding.FragmentMyPageBinding
+import com.ssafy.challenmungs.presentation.base.BaseFragment
+
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+
+    override fun initView() {
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
