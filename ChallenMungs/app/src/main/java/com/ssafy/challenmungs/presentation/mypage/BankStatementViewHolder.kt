0aadff311a1019e2c77782c327f0e6cdfc1229c3
이아름 <souk0712@naver.com>
2023-03-22 20:39:06 +0900

    [FEAT] 내 지갑 layout 구현

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/BankStatementViewHolder.kt
@@ -0,0 +1,38 @@
+package com.ssafy.challenmungs.presentation.mypage
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.challenmungs.databinding.ItemBankStatementBottomBinding
+import com.ssafy.challenmungs.databinding.ItemBankStatementMiddleBinding
+import com.ssafy.challenmungs.databinding.ItemBankStatementTopBinding
+import com.ssafy.challenmungs.domain.entity.BankStatementItem
+
+sealed class BankStatementViewHolder(binding: ViewDataBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    abstract fun onBind(item: BankStatementItem)
+
+    class TopItemViewHolder(private val binding: ItemBankStatementTopBinding) :
+        BankStatementViewHolder(binding) {
+
+        override fun onBind(item: BankStatementItem) {
+
+        }
+    }
+
+    class MiddleItemViewHolder(private val binding: ItemBankStatementMiddleBinding) :
+        BankStatementViewHolder(binding) {
+
+        override fun onBind(item: BankStatementItem) {
+
+        }
+    }
+
+    class BottomItemViewHolder(private val binding: ItemBankStatementBottomBinding) :
+        BankStatementViewHolder(binding) {
+
+        override fun onBind(item: BankStatementItem) {
+
+        }
+    }
+}
