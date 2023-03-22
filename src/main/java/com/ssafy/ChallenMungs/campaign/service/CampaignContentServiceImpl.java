0d2fb2175a7abf7fabc16a54edadd5d961150a72
choijiwon <gwonwon1201@gmail.com>
2023-03-22 21:46:50 +0900

    [FEAT] 캠페인 생성, 디테일 보기 추가 S08P22D210-185

--- src/main/java/com/ssafy/ChallenMungs/campaign/service/CampaignContentServiceImpl.java
+++ src/main/java/com/ssafy/ChallenMungs/campaign/service/CampaignContentServiceImpl.java
@@ -1,8 +1,24 @@
 package com.ssafy.ChallenMungs.campaign.service;
 
+import com.ssafy.ChallenMungs.Test.dto.TestDto;
 import com.ssafy.ChallenMungs.campaign.dto.CampaignDetailDto;
 import com.ssafy.ChallenMungs.campaign.dto.CampaignInsertDto;
+import com.ssafy.ChallenMungs.campaign.dto.ContentDto;
+import com.ssafy.ChallenMungs.campaign.entity.Campaign;
+import com.ssafy.ChallenMungs.campaign.entity.Content;
+import com.ssafy.ChallenMungs.campaign.repository.CampaignContentRepository;
+import com.ssafy.ChallenMungs.campaign.repository.CampaignListRepository;
+import com.ssafy.ChallenMungs.user.controller.UserController;
+import com.ssafy.ChallenMungs.user.entity.User;
+import com.ssafy.ChallenMungs.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -11,9 +27,58 @@ import javax.transaction.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class CampaignContentServiceImpl implements CampaignContentService{
+
+    private final CampaignListRepository listRepo;
+    private final CampaignContentRepository contentRepo;
+    private final UserRepository userRepo;
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
     @Override
     public void createCampaign(CampaignInsertDto info) {
+        //캠페인 개요 저장하고
+        Campaign campaign=initCampaign(info);
+        listRepo.save(campaign);
+        listRepo.flush(); //방금 막 넣은 엔티티의 아이디를 가져오기 위함
 
+        //내용 저장하기
+        for(int i=0;i<info.getContentList().size();i++){
+            Content content=initContent(info.getContentList().get(i));
+            content.setSeq(i);
+            content.setCampaign(campaign);
+            contentRepo.save(content);
+        }
+
+    }
+    public Content initContent(ContentDto dto){
+        Content content=new Content();
+        content.setBody(dto.getBody());
+        content.setType(dto.getType());
+        return content;
+    }
+    public Campaign initCampaign(CampaignInsertDto info){
+        User shelter=getUser(info.getLoginId());
+        String account=getAccount(info.getLoginId());
+        Campaign campaign=new Campaign();
+        campaign.setUser(shelter);
+        campaign.setName(shelter.getName());
+        campaign.setWalletAddress(account);
+        campaign.setThumbnail(info.getThumbnail());
+        campaign.setTitle(info.getTitle());
+        campaign.setTargetAmount(info.getTargetAmount());
+        campaign.setRegistDate(LocalDateTime.now());
+        campaign.setCollectAmount(0);
+        campaign.setWithdrawAmount(0);
+        campaign.setEnd(false);
+
+        return campaign;
+    }
+
+    public User getUser(String loginId){
+        return userRepo.findUserByLoginId(loginId);
+    }
+
+    //todo 지갑
+    public String getAccount(String loginId){
+        return "testAccount";
     }
 
     @Override
@@ -28,6 +93,23 @@ public class CampaignContentServiceImpl implements CampaignContentService{
 
     @Override
     public CampaignDetailDto viewDetailCampaign(int campaignId) {
-        return null;
+        Campaign campaign=listRepo.findCampaignByCampaignId(campaignId);
+        int loveCnt=0;
+        CampaignDetailDto result=new CampaignDetailDto(
+                campaign.getTitle(),campaign.getThumbnail(), campaign.getName(),0,
+                campaign.getCollectAmount(),campaign.getTargetAmount(),getContentDtoList(campaign));
+
+        return result;
+
+    }
+    public List<ContentDto> getContentDtoList(Campaign campaign){
+        List<Content> list=contentRepo.findAllByCampaign(campaign);
+        List <ContentDto> contentDtoList=new ArrayList<>();
+        for(Content content:list){
+            ContentDto dto=new ContentDto(content.getType(),content.getBody());
+            contentDtoList.add(dto);
+        }
+        return contentDtoList;
+
     }
 }
