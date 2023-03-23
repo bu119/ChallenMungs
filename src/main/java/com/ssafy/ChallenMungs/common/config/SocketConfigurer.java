cd0fa7a564d90d8a6020445012ac183a166c0185
kimbugyeong <sa01023@naver.com>
2023-03-23 14:28:00 +0900

    [FEAT]일반챌린지

--- src/main/java/com/ssafy/ChallenMungs/common/config/SocketConfigurer.java
+++ src/main/java/com/ssafy/ChallenMungs/common/config/SocketConfigurer.java
@@ -1,19 +1,19 @@
-package com.ssafy.ChallenMungs.common.config;
-
-import com.ssafy.ChallenMungs.challenge.handler.PanelSocketHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-@Configuration
-@EnableWebSocket
-public class SocketConfigurer implements WebSocketConfigurer {
-    @Autowired
-    PanelSocketHandler socketHandler;
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(socketHandler, "/panelSocket").setAllowedOrigins("*");
-    }
-}
+//package com.ssafy.ChallenMungs.common.config;
+//
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.config.annotation.EnableWebSocket;
+//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+//
+//@Configuration
+//@EnableWebSocket
+//public class SocketConfigurer implements WebSocketConfigurer {
+//    @Autowired
+//    PanelSocketHandler socketHandler;
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        registry.addHandler(socketHandler, "/panelSocket").setAllowedOrigins("*");
+//    }
+//}
