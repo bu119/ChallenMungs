0625ccb6343e8c46b4cd76d6fee271fbd6c6b303
이동하 <siganshoyou@gmail.com>
2023-03-23 15:51:57 +0900

    [FEAT]챌린지엔티티간상속관계확립

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralController.java
@@ -1,14 +1,23 @@
 package com.ssafy.ChallenMungs.challenge.general.controller;
 
+import com.ssafy.ChallenMungs.challenge.general.entity.General;
+import com.ssafy.ChallenMungs.challenge.general.repository.GeneralRepository;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/SpecialChallenge")
 @CrossOrigin("*")
 @Api(value = "challenge", description = "챌린지를 관리하는 컨트롤러에요!")
 public class GeneralController {
-    
+    @Autowired
+    GeneralRepository generalRepository;
+
+    @GetMapping("/dddd")
+    void dddd() {
+        generalRepository.save(General.builder().build());
+    }
 }
