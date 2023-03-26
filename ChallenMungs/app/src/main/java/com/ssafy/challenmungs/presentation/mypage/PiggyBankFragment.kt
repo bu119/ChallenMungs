6ab358b83047c5f17d2c2c6a5201b1ab90b1dc0c
이아름 <souk0712@naver.com>
2023-03-26 16:37:42 +0900

    [REFACTOR] fragment_my_wallet.xml과 fragment_piggy_bank.xml 병합 (#S08P22D210-199)

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/PiggyBankFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/PiggyBankFragment.kt
@@ -1,12 +1,19 @@
 package com.ssafy.challenmungs.presentation.mypage
 
+import android.view.View
 import com.ssafy.challenmungs.R
-import com.ssafy.challenmungs.databinding.FragmentPiggyBankBinding
+import com.ssafy.challenmungs.databinding.FragmentMyWalletBinding
 import com.ssafy.challenmungs.presentation.base.BaseFragment
 
-class PiggyBankFragment : BaseFragment<FragmentPiggyBankBinding>(R.layout.fragment_piggy_bank) {
+class PiggyBankFragment : BaseFragment<FragmentMyWalletBinding>(R.layout.fragment_my_wallet) {
 
     override fun initView() {
-
+        binding.apply {
+            tvAddressCopy.visibility = View.GONE
+            toolbar.tvTitle.text = getString(R.string.title_donate_bank)
+            tvCurrentBalanceTitle.text = getString(R.string.content_donation_possible_amount)
+            btnParticipation.text = getString(R.string.content_donate)
+        }
     }
+
 }
