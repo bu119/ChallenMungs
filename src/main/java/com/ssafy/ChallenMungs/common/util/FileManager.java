f5749859dcbd425207eaeeadef50e3aacacce2be
이동하 <siganshoyou@gmail.com>
2023-03-28 00:13:59 +0900

    [FIX]null3

--- src/main/java/com/ssafy/ChallenMungs/common/util/FileManager.java
+++ src/main/java/com/ssafy/ChallenMungs/common/util/FileManager.java
@@ -26,8 +26,11 @@ public class FileManager {
         if (System.getProperty("os.name").substring(0, 3).equals("Win")) directoryPath = windowsPath;
         else directoryPath = ubuntuPath;
         System.out.println(directoryPath);
-        File directory = new File("/home/asset");
+        File directory = new File(directoryPath);
         System.out.println("디렉터리:::" + directory + "  " + directory.exists());
+        if (!directory.exists()) directory.mkdirs();
+        File directory2 = new File("/home");
+        System.out.println("디렉터리:::" + directory2 + "  " + directory2.exists());
 //        File[] files = directory.listFiles();
 //        System.out.println("파일스:::" + files);
 //        for (File file : files) {
