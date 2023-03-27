521c6a1030f97960e6415bed95e48532c1ab82ba
dobbi030 <dmstjs7047@hanmail.net>
2023-03-28 02:07:03 +0900

    [FEAT] 권한 체크 구현
    권한 허용 여부에 따른 분기 코드 미구현

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/PermissionActivity.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/PermissionActivity.kt
@@ -1,7 +1,10 @@
 package com.ssafy.challenmungs.presentation
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.ssafy.challenmungs.ApplicationClass
 import com.ssafy.challenmungs.common.util.setImmersiveMode
 import com.ssafy.challenmungs.databinding.ActivityPermissionBinding
@@ -16,5 +19,39 @@ class PermissionActivity : AppCompatActivity() {
         setContentView(binding.root)
         setImmersiveMode()
         ApplicationClass.preferences.isFirstRun = false
+
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.btnPermissionCheck.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this@PermissionActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // 로그인 체크 후 화면 분기
+                }
+//                shouldShowRequestPermissionRationale()
+            }
+
+            when {
+                ContextCompat.checkSelfPermission(
+                    this@PermissionActivity,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED -> {
+
+                }
+            }
+
+            when {
+                ContextCompat.checkSelfPermission(
+                    this@PermissionActivity,
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) == PackageManager.PERMISSION_GRANTED -> {
+
+                }
+            }
+        }
     }
 }
