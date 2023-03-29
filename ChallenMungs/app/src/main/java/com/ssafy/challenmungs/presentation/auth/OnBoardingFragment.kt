251e431ce69a63c4f297de02c51f8fc82c055abf
dobbi030 <dmstjs7047@hanmail.net>
2023-03-30 03:59:23 +0900

    [FEAT] 뒤로가기를 하거나 EditText가 아닌 부분을 눌렀을 때 EditText의 focus가 해제되는 기능 추가
    뒤로가기를 눌렀을 때 상하단 바로 인해 뒤로가기 버튼이 인식되지 않아 focus가 해제되지 않는 문제가 있음

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/OnBoardingFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/OnBoardingFragment.kt
@@ -1,6 +1,10 @@
 package com.ssafy.challenmungs.presentation.auth
 
+import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import com.ssafy.challenmungs.R
@@ -17,6 +21,7 @@ class OnBoardingFragment : BaseFragment<FragmentOnBoardingBinding>(R.layout.frag
     private val authViewModel by activityViewModels<AuthViewModel>()
     private val memberViewModel by activityViewModels<MemberViewModel>()
     private val walletViewModel by activityViewModels<WalletViewModel>()
+    private val callback = focus()
 
     override fun initView() {
         binding.toolbar.tvTitle.text = getString(R.string.title_member_onboarding)
@@ -24,15 +29,30 @@ class OnBoardingFragment : BaseFragment<FragmentOnBoardingBinding>(R.layout.frag
         observe()
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        callback.remove()
+    }
+
     private fun initListener() {
         binding.btnSave.setOnClickListener {
             if (authViewModel.accessToken.value != null) {
-                authViewModel.requestJoin(binding.etNickname.text.toString())
+                authViewModel.requestJoin(binding.tilEtNickname.text.toString())
             }
         }
 
         binding.toolbar.ivBack.setOnClickListener {
             popBackStack()
+        }
+
+        binding.root.setOnClickListener {
+            hideKeyboard()
+            it.clearFocus()
         }
     }
 
@@ -60,15 +80,36 @@ class OnBoardingFragment : BaseFragment<FragmentOnBoardingBinding>(R.layout.frag
                         walletViewModel.address.value!![1]
                     )
 
-                    if (result)
-                        moveToHomeActivity()
+                    if (result) {
+                        val intent = Intent(activity, HomeActivity::class.java)
+                        startActivity(intent)
+                        requireActivity().finish()
+                    }
                 }
         }
     }
 
-    private fun moveToHomeActivity() {
-        val intent = Intent(activity, HomeActivity::class.java)
-        startActivity(intent)
-        requireActivity().finish()
+    private fun focus(): OnBackPressedCallback {
+
+        return object : OnBackPressedCallback(true) {
+
+            override fun handleOnBackPressed() {
+                Log.d(
+                    "OnBoardingFragment",
+                    "OnBoardingFragment: focused(${binding.tilEtNickname.isFocused})"
+                )
+
+                if (binding.tilEtNickname.isFocused) {
+                    hideKeyboard()
+//                    binding.root.clearFocus()
+                }
+            }
+        }
+    }
+
+    private fun hideKeyboard() {
+        val imm =
+            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 }
