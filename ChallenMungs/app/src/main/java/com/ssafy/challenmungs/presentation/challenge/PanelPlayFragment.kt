6c8325cdcb80f886dff837231bcea22a02427488
이아름 <souk0712@naver.com>
2023-04-01 21:26:29 +0900

    [FEAT] 내 위치 표시

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/PanelPlayFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/PanelPlayFragment.kt
@@ -1,21 +1,29 @@
 package com.ssafy.challenmungs.presentation.challenge
 
+import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
-import android.graphics.drawable.VectorDrawable
+import android.util.Log
+import android.widget.Toast
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.toBitmap
+import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import com.ssafy.challenmungs.R
-import com.ssafy.challenmungs.common.util.Map.createRectangle
-import com.ssafy.challenmungs.common.util.Map.defaultPosition
+import com.ssafy.challenmungs.common.util.MapHelper.DISTANCE
+import com.ssafy.challenmungs.common.util.MapHelper.checkLocationServicesStatus
+import com.ssafy.challenmungs.common.util.MapHelper.createRectangle
+import com.ssafy.challenmungs.common.util.MapHelper.defaultPosition
+import com.ssafy.challenmungs.common.util.MapHelper.locationRequest
+import com.ssafy.challenmungs.common.util.MapHelper.replaceDrawableMarker
+import com.ssafy.challenmungs.common.util.PermissionHelper
 import com.ssafy.challenmungs.common.util.px
 import com.ssafy.challenmungs.databinding.FragmentPanelPlayBinding
 import com.ssafy.challenmungs.presentation.base.BaseFragment
@@ -25,18 +33,49 @@ import dagger.hilt.android.AndroidEntryPoint
 class PanelPlayFragment : BaseFragment<FragmentPanelPlayBinding>(R.layout.fragment_panel_play),
     OnMapReadyCallback {
 
+    private val panelPlayViewModel by activityViewModels<PanelPlayViewModel>()
+    private lateinit var profileImg: String
     private val panels = mutableListOf<Polygon>()
     private var myMarker: Marker? = null
+    private lateinit var map: GoogleMap
+    private val mFusedLocationClient: FusedLocationProviderClient by lazy {
+        LocationServices.getFusedLocationProviderClient(requireActivity())
+    }
+    private lateinit var currentPosition: LatLng
+
+    //위치정보 요청시 호출
+    private var locationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            val locationList = locationResult.locations
+            if (locationList.size > 0) {
+                val location = locationList[locationList.size - 1]
+                currentPosition = LatLng(location.latitude, location.longitude)
+                Log.d(TAG, "onLocationResult: 위도: ${location.latitude}, 경도: ${location.longitude}")
+
+                createMyMarker(map, profileImg)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, DEFAULT_ZOOM))
+            }
+        }
+    }
 
     override fun initView() {
         val mapFragment =
             childFragmentManager.findFragmentById(R.id.fcv_google_map) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
+
+        profileImg = getString(R.string.default_profile_url)
+
+        binding.btnWalk.setOnClickListener {
+            Toast.makeText(requireContext(), "clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        with(googleMap) {
-            moveCamera(CameraUpdateFactory.newLatLngZoom(defaultPosition, 15f))
+        map = googleMap
+        setMyLocation()
+        with(map) {
+            moveCamera(CameraUpdateFactory.newLatLngZoom(defaultPosition, DEFAULT_ZOOM))
             // 세로
             for (i in -10..10) {
                 for (j in -10..10) {
@@ -48,9 +87,33 @@ class PanelPlayFragment : BaseFragment<FragmentPanelPlayBinding>(R.layout.fragme
                     )
                 }
             }
+        }
+    }
 
-            val profileImg = getString(R.string.default_profile_url)
-            createMyMarker(this, profileImg)
+    // 내 위치 설정
+    @SuppressLint("MissingPermission")
+    private fun setMyLocation() {
+        // 위치서비스 활성화 여부 check
+        if (!checkLocationServicesStatus(activity)) {
+            Toast.makeText(
+                context,
+                "위치 서비스가 꺼져 있어, 현재 위치를 확인할 수 없습니다.",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            if (PermissionHelper.hasLocationPermission(requireContext())) {
+                mFusedLocationClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    null
+                )
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.location_permission_warning_message),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
@@ -58,7 +121,7 @@ class PanelPlayFragment : BaseFragment<FragmentPanelPlayBinding>(R.layout.fragme
         val rectOptions = PolygonOptions().apply {
             addAll(createRectangle(center, DISTANCE))
             fillColor(ContextCompat.getColor(requireContext(), fillColorArgb))
-            strokeWidth(0f)
+            strokeWidth(1f)
         }
         panels.add(googleMap.addPolygon(rectOptions))
     }
@@ -80,7 +143,7 @@ class PanelPlayFragment : BaseFragment<FragmentPanelPlayBinding>(R.layout.fragme
                     myMarker?.remove()
                     myMarker = googleMap.addMarker(
                         MarkerOptions()
-                            .position(defaultPosition)
+                            .position(currentPosition)
                             .icon(BitmapDescriptorFactory.fromBitmap(bitmap))
                             .anchor(0.5f, 0.5f)
                     )
@@ -88,35 +151,40 @@ class PanelPlayFragment : BaseFragment<FragmentPanelPlayBinding>(R.layout.fragme
 
                 override fun onLoadStarted(placeholder: Drawable?) {
                     super.onLoadStarted(placeholder)
-                    replaceDrawableMarker(googleMap, placeholder)
+                    myMarker?.remove()
+
+                    myMarker = replaceDrawableMarker(googleMap, placeholder, currentPosition)
                 }
 
                 override fun onLoadFailed(errorDrawable: Drawable?) {
                     super.onLoadFailed(errorDrawable)
-                    replaceDrawableMarker(googleMap, errorDrawable)
+                    myMarker?.remove()
+                    myMarker = replaceDrawableMarker(googleMap, errorDrawable, currentPosition)
                 }
 
                 override fun onLoadCleared(placeholder: Drawable?) {
-                    replaceDrawableMarker(googleMap, placeholder)
+                    myMarker?.remove()
+                    myMarker = replaceDrawableMarker(googleMap, placeholder, currentPosition)
                 }
             })
     }
 
-    fun replaceDrawableMarker(googleMap: GoogleMap, drawable: Drawable?) {
-        val bitmapDrawable = drawable as VectorDrawable
-        val bitmap = bitmapDrawable.toBitmap()
-        myMarker?.remove()
-        myMarker = googleMap.addMarker(
-            MarkerOptions()
-                .position(defaultPosition)
-                .icon(
-                    BitmapDescriptorFactory.fromBitmap(bitmap)
-                )
-                .anchor(0.5f, 0.5f)
-        )
+
+    @SuppressLint("MissingPermission")
+    override fun onStart() {
+        super.onStart()
+        if (PermissionHelper.hasLocationPermission(requireContext())) {
+            mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mFusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
     companion object {
-        private const val DISTANCE = 0.001
+        private const val TAG = "PanelPlayFragment_챌린멍스"
+        private const val DEFAULT_ZOOM = 15f
     }
 }
