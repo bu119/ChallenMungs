326f3ab058a36987e7085b5a1c6a4c84cec04aa1
dobbi030 <dmstjs7047@hanmail.net>
2023-04-01 16:42:21 +0900

    [FEAT] 챌린지 홈 화면 layout 구현

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/ChallengeFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/ChallengeFragment.kt
@@ -1,5 +1,6 @@
 package com.ssafy.challenmungs.presentation.challenge
 
+import androidx.recyclerview.widget.GridLayoutManager
 import com.ssafy.challenmungs.R
 import com.ssafy.challenmungs.databinding.FragmentChallengeBinding
 import com.ssafy.challenmungs.presentation.base.BaseFragment
@@ -7,6 +8,14 @@ import com.ssafy.challenmungs.presentation.base.BaseFragment
 class ChallengeFragment : BaseFragment<FragmentChallengeBinding>(R.layout.fragment_challenge) {
 
     override fun initView() {
+        initRecyclerView()
+    }
 
+    private fun initRecyclerView() {
+        binding.rvChallenge.apply {
+            adapter = ChallengeListAdapter(arrayListOf())
+            layoutManager =
+                GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+        }
     }
 }
