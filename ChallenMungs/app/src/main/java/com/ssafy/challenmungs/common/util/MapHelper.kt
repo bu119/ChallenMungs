2f09a43bc9d1ff5b2f5ca8c2d6543f682e347204
이아름 <souk0712@naver.com>
2023-04-03 23:14:40 +0900

    [CHORE] replaceDrawableMarker 함수 이미지 크기 조정

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/common/util/MapHelper.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/common/util/MapHelper.kt
@@ -1,6 +1,8 @@
 package com.ssafy.challenmungs.common.util
 
 import android.app.Activity
+import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.VectorDrawable
 import android.location.LocationManager
@@ -18,7 +20,6 @@ object MapHelper {
     const val UPDATE_INTERVAL = 1000L // 1초
     const val MIN_UPDATE_INTERVAL = 500L // 0.5초
     const val MIN_UPDATE_DISTANCE = 10f // 10m
-    const val DISTANCE = 0.00035
     val defaultPosition = LatLng(36.107102, 128.416558)
 
     val locationRequest: LocationRequest by lazy {
@@ -50,15 +51,19 @@ object MapHelper {
     fun replaceDrawableMarker(
         googleMap: GoogleMap,
         drawable: Drawable?,
-        position: LatLng
+        position: LatLng,
+        context: Context
     ): Marker? {
         val bitmapDrawable = drawable as VectorDrawable
         val bitmap = bitmapDrawable.toBitmap()
+        val pixels = 40.px(context)
+        val newBitmap = Bitmap.createScaledBitmap(bitmap, pixels, pixels, true)
+
         return googleMap.addMarker(
             MarkerOptions()
                 .position(position)
                 .icon(
-                    BitmapDescriptorFactory.fromBitmap(bitmap)
+                    BitmapDescriptorFactory.fromBitmap(newBitmap)
                 )
                 .anchor(0.5f, 0.5f)
         )
