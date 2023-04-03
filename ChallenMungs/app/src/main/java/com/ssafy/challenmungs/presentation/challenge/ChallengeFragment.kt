2052eb909265e0ac0113442cd957f438d004008a
dobbi030 <dmstjs7047@hanmail.net>
2023-04-03 17:24:27 +0900

    [FEAT] recyclerview에 아이템 연결

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/ChallengeFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/ChallengeFragment.kt
@@ -1,5 +1,7 @@
 package com.ssafy.challenmungs.presentation.challenge
 
+import android.view.KeyEvent
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.ssafy.challenmungs.R
 import com.ssafy.challenmungs.common.util.GridItemDecoration
@@ -8,16 +10,41 @@ import com.ssafy.challenmungs.presentation.base.BaseFragment
 
 class ChallengeFragment : BaseFragment<FragmentChallengeBinding>(R.layout.fragment_challenge) {
 
+    private val challengeViewModel by activityViewModels<ChallengeViewModel>()
+    private val challengeListAdapter by lazy { ChallengeListAdapter(requireContext()) }
+
     override fun initView() {
         initRecyclerView()
+        observe()
+        getChallengeList()
     }
 
     private fun initRecyclerView() {
         binding.rvChallenge.apply {
-            adapter = ChallengeListAdapter(arrayListOf())
+            challengeListAdapter.submitList(arrayListOf())
+            adapter = challengeListAdapter
             layoutManager =
                 GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
-            addItemDecoration(GridItemDecoration(requireContext(), 2, 20, 10))
+            addItemDecoration(GridItemDecoration(requireContext(), 2, 15, 10))
         }
+
+        binding.tilEtSearch.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                challengeViewModel.getChallengeList(1, v.toString())
+            }
+            return@setOnKeyListener true
+        }
+    }
+
+    private fun observe() {
+        challengeViewModel.challengeList.observe(viewLifecycleOwner) {
+            it?.let {
+                challengeListAdapter.submitList(it)
+            }
+        }
+    }
+
+    private fun getChallengeList() {
+        challengeViewModel.getChallengeList(1)
     }
 }
