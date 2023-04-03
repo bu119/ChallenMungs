f81aa543e7de57a71703b58e7744943854be08d2
dobbi030 <dmstjs7047@hanmail.net>
2023-04-03 17:25:36 +0900

    [CHORE] item type을 ChallengeCard에서 Challenge로 변경

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/MyChallengeListFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/MyChallengeListFragment.kt
@@ -2,13 +2,13 @@ package com.ssafy.challenmungs.presentation.mypage
 
 import com.ssafy.challenmungs.R
 import com.ssafy.challenmungs.databinding.FragmentMyChallengeListBinding
-import com.ssafy.challenmungs.domain.entity.challenge.ChallengeCard
+import com.ssafy.challenmungs.domain.entity.challenge.Challenge
 import com.ssafy.challenmungs.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MyChallengeListFragment(
-    private val position: Int, private val dataList: ArrayList<ChallengeCard>
+    private val position: Int, private val dataList: ArrayList<Challenge>
 ) : BaseFragment<FragmentMyChallengeListBinding>(R.layout.fragment_my_challenge_list) {
 
     override fun initView() {
