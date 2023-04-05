02a1a39f6d35b04f54ffc9d5aace7877b8e9f28f
변윤경 <forever4410@gmail.com>
2023-04-05 09:09:54 +0900

    Update WalletRepository.java

--- src/main/java/com/ssafy/ChallenMungs/blockchain/repository/WalletRepository.java
+++ src/main/java/com/ssafy/ChallenMungs/blockchain/repository/WalletRepository.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
 
-   String normalChallenge = "0x2649eadC4C15bac554940A0A702fa759bddf0dBe";
-   String specialChallenge = "0xee43BB5476e52B04175d698C56cC4516b96A85A5";
    List <Wallet> findAllByUser(User user);
 
    Wallet findByUserAndType(User user,char type);
