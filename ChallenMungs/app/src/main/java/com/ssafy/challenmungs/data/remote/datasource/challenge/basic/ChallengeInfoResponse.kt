e0f5c737e535a0019118c1de08b2725815c32618
dobbi030 <dmstjs7047@hanmail.net>
2023-04-06 00:23:41 +0900

    [CHORE] 챌린지 정보 페이지에 맞게 entity 변경

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/datasource/challenge/basic/ChallengeInfoResponse.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/datasource/challenge/basic/ChallengeInfoResponse.kt
@@ -2,18 +2,22 @@ package com.ssafy.challenmungs.data.remote.datasource.challenge.basic
 
 import com.google.gson.annotations.SerializedName
 import com.ssafy.challenmungs.data.remote.datasource.base.DataToDomainMapper
-import com.ssafy.challenmungs.domain.entity.challenge.NotStartedChallenge
+import com.ssafy.challenmungs.domain.entity.challenge.NotStartedChallengeDetail
 
 data class ChallengeInfoResponse(
     @SerializedName("challenge")
     val challenge: ChallengeResponse,
     @SerializedName("participant")
     val participant: List<ParticipantResponse>
-) : DataToDomainMapper<NotStartedChallenge> {
+) : DataToDomainMapper<NotStartedChallengeDetail> {
 
-    override fun toDomainModel(): NotStartedChallenge = NotStartedChallenge(
+    override fun toDomainModel(): NotStartedChallengeDetail = NotStartedChallengeDetail(
         challengeId = challenge.challengeId,
-        challengeType = challenge.challengeType,
+        challengeType = when (challenge.challengeType) {
+            1 -> "일반 챌린지"
+            2 -> "판넬 뒤집기"
+            else -> "보물 찾기"
+        },
         title = challenge.title,
         startDate = challenge.startDate.slice(IntRange(5, challenge.startDate.length - 1)),
         endDate = challenge.endDate.slice(IntRange(5, challenge.endDate.length - 1)),
@@ -24,7 +28,9 @@ data class ChallengeInfoResponse(
         campaignId = challenge.campaignId,
         successCondition = challenge.successCondition,
         description = challenge.description,
-        gameType = challenge.gameType,
+        gameType = if (challenge.gameType != null) {
+            if (challenge.gameType == 1) "개인전" else "팀전"
+        } else null,
         centerLat = challenge.centerLat,
         centerLng = challenge.centerLng,
         cellD = challenge.cellD,
@@ -34,6 +40,6 @@ data class ChallengeInfoResponse(
         minLng = challenge.minLng,
         cellSize = challenge.cellSize,
         mapSize = challenge.mapSize,
-        participants = participant.map { it.toDomainModel() }
+        participants = ArrayList(participant.map { it.toDomainModel() })
     )
 }
